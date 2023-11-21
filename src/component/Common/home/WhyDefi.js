1839170dc556d63278c2514fe9a00b4fc9dc7dcd
follyb2810 <babatundeyusuffolorunsho@gmail.com>
2023-11-20 23:58:09 -0800

    adding chages

--- src/component/Common/home/WhyDefi.js
+++ src/component/Common/home/WhyDefi.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Template from './../../Shared/Template';
 import { Card, Col, Row, Stack } from 'react-bootstrap';
 import { templatetHead, whyDefiContent } from '../../data';
-import { Bounce, Fade } from 'react-reveal';
+
 
 const WhyDefi = () => {
   const whyDefiData = templatetHead[1];
@@ -14,23 +14,23 @@ const WhyDefi = () => {
         <Row className='align-items-center container-fluid'>
           <Col sm={12} md={4} className=' '>
             <section className='d-flex flex-row justify-content-center flex-md-column '>
-            <Bounce right cascade>
+            
             <img src={img1} className='whyDefiImage' alt='' />
             <img src={img2} className='whyDefiImage' alt='' />
-            </Bounce>
+            
             </section>
           </Col>
           <Col sm={12} md={8} className='container-fluid'>
             <Stack>
               {whyDefiContent.map((content, index) => (
                 <Stack direction='horizontal' className='text-start  border-bottom border-5 p-2 ' key={index}>
-                <Fade left cascade>
+                
                   <div className='item-first text-wrap fs-why'>{content.itemFirst}</div>
                   <div className='item-second-last text-center fs-why ' style={{color:content.itemSecond==='Instant Crypto Credit Lines'?'blue':'green'}}>
                     {content.itemSecond}
                   </div>
                   <div className='item-second-last text-center fs-why' style={{color:content.itemLast==='Conventional Loans'?'blue':''}}>{content.itemLast}</div>
-                  </Fade>
+                  
                   </Stack>
               ))}
             </Stack>
